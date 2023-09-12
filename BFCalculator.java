b63c20d19d0e4f35e22cb362a336088df130b41a
Wenfei Lin <wl688728@gmail.com>
2023-09-11 21:41:25 -0500

    Implement store function and make it interact with evaluate function in QuickCalculator

--- BFCalculator.java
+++ BFCalculator.java
@@ -1,4 +1,4 @@
-//import java.io.PrintWriter;
+import java.io.PrintWriter;
 
 public class BFCalculator {
 
@@ -9,30 +9,48 @@ public class BFCalculator {
   // constructor
   public BFCalculator() {
     this.result = new BigFraction(0, 0);
-    this.storeArr = new BigFraction[26];
+    this.storeArr = new BigFraction[52];
   }
 
   // methods
   public void evaluate (String exp) {
     String[] expressionParts = exp.split(" ");
+    BigFraction operand1 = new BigFraction("0/0");
+    String operation = "";
+    BigFraction operand2 = new BigFraction("0/0");
+
+    char possibleRegister1 = expressionParts[0].charAt(0);
+    char possibleRegister2 = expressionParts[2].charAt(0);
+
+    if (possibleRegister1 >= 'A' && possibleRegister1 <= 'Z') {
+      if (!this.storeArr[(int) possibleRegister1 - 65].equals(null)) {
+        operand1 = this.storeArr[(int) possibleRegister1 - 65];
+      }
+    } else if (possibleRegister1 >= 'a' && possibleRegister1 <= 'z') {
+      if (!this.storeArr[(int) possibleRegister1 - 97 + 26].equals(null)) {
+        operand1 = this.storeArr[(int) possibleRegister1 - 97 + 26];
+      }
+    } else {
+      operand1 = new BigFraction(expressionParts[0]);
+    }
     
-    BigFraction operand1;
-    BigFraction operand2;
-    String operation;
-    //PrintWriter pen = new PrintWriter(System.out, true);
+    if (possibleRegister2 >= 'A' && possibleRegister2 <= 'Z') {
+      if (!this.storeArr[(int) possibleRegister2 - 65].equals(null)) {
+        operand2 = this.storeArr[(int) possibleRegister2 - 65];
+      }
+    } else if (possibleRegister2 >= 'a' && possibleRegister2 <= 'z') {
+      if (!this.storeArr[(int) possibleRegister2 - 97 + 26].equals(null)) {
+        operand2 = this.storeArr[(int) possibleRegister2 - 97 + 26];
+      }
+    } else {
+      operand2 = new BigFraction(expressionParts[2]);
+    }
     
-    operand1 = new BigFraction(expressionParts[0]);
     operation = expressionParts[1];
-    operand2 = new BigFraction(expressionParts[2]);
-    //System.out.println("this?");
 
-    System.out.println(operand1 + " " + operation + " " + operand2);
-
-    //System.out.println("hey");
     switch (operation) {
       case "+" :
         this.result = operand1.add(operand2);
-        //System.out.println("hey");
         break;
       case "-" :
         this.result = operand1.subtract(operand2);
@@ -44,7 +62,8 @@ public class BFCalculator {
         this.result = operand1.multiply(operand2);
         break;
       default :
-        System.out.println("error");
+        System.out.println("Invalid operator");
+        System.exit(2);
     }
 
     this.result = this.result.simplify();
@@ -52,7 +71,18 @@ public class BFCalculator {
   }
 
   public void store (char register) {
-    
+    int index = -1;
+
+    if (register >= 'A' && register <= 'Z') {
+      index = (int) register - 65;
+    } else if (register >= 'a' && register <= 'z') {
+      index = (int) register - 97 + 26;
+    } else {
+      PrintWriter errorPrinter = new PrintWriter(System.out, true);
+      errorPrinter.println("The register must be a letter of the alphabet");
+      System.exit(1);
+    }
+    this.storeArr[index]= this.result;
   }
 
 }
--- QuickCalculator.java
+++ QuickCalculator.java
@@ -3,33 +3,40 @@ import java.io.PrintWriter;
 public class QuickCalculator {
   public static void main (String[] args) {
     BFCalculator Calculator = new BFCalculator();
+    PrintWriter errorPrinter = new PrintWriter(System.out, true);
     PrintWriter pen = new PrintWriter(System.out, true);
-    String[] exprParts = args[0].split(" ");
 
-    if (args[0].equals("STORE")) {
-      //System.out.println("meep");
-    } else { // Evaluation
-
-      int firstOperandIndex = 0;
-      int operatorIndex = 1;
-      int secondOperandIndex = 2;
-
-      while (secondOperandIndex <= exprParts.length - 1) {
-        if (operatorIndex == 1) {
-          //System.out.println(firstOperandIndex + " " + operatorIndex + " " + secondOperandIndex);
-          //System.out.println(exprParts[firstOperandIndex] + " " + exprParts[operatorIndex] + " " + exprParts[secondOperandIndex]);
-          String expr = exprParts[firstOperandIndex] + " " + exprParts[operatorIndex] + " " + exprParts[secondOperandIndex];
-          Calculator.evaluate(expr);
+    for (int i = 0; i < args.length; i++){
+      String[] exprParts = args[i].split(" ");
+  
+      if (exprParts[0].equals("STORE")) {
+        String registerStr = exprParts[1];
+        if (registerStr.length() == 1) {
+          Calculator.store(registerStr.charAt(0));
         } else {
-          //System.out.println(firstOperandIndex + " " + operatorIndex + " " + secondOperandIndex);
-          String expr = Calculator.result + " " + exprParts[operatorIndex] + " " + exprParts[secondOperandIndex];
-          Calculator.evaluate(expr);
+          errorPrinter.println("The register must be a letter of the alphabet");
+          System.exit(1);
         }
-        operatorIndex += 2;
-        secondOperandIndex += 2;
+      } else { // Evaluation
+        
+        int firstOperandIndex = 0;
+        int operatorIndex = 1;
+        int secondOperandIndex = 2;
+  
+        while (secondOperandIndex <= exprParts.length - 1) {
+          if (operatorIndex == 1) {
+            String expr = exprParts[firstOperandIndex] + " " + exprParts[operatorIndex] + " " + exprParts[secondOperandIndex];
+            Calculator.evaluate(expr);
+            pen.println(expr + " = " + Calculator.result);
+          } else {
+            String expr = Calculator.result + " " + exprParts[operatorIndex] + " " + exprParts[secondOperandIndex];
+            Calculator.evaluate(expr);
+            pen.println(expr + " = " + Calculator.result);
+          }
+          operatorIndex += 2;
+          secondOperandIndex += 2;
+        }
       }
-
-      pen.println(Calculator.result);
     }
   }
 }
